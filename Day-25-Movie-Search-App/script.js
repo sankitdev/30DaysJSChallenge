23d3a33ffbe924020ba8db517a5cc68684eabd3b
Ankit Singh <ankitsingh635101@gmail.com>
2024-08-12 20:52:45 +0530

    basic ui with movie search functionality

--- Day-25-Movie-Search-App/script.js
+++ Day-25-Movie-Search-App/script.js
@@ -1,6 +1,8 @@
 const API = "41357241";
 const searchIcon = document.querySelector("#searchIcon");
 const search = document.querySelector("#search");
+const container = document.querySelector(".container");
+const popupContainer = document.querySelector(".popupContainer");
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
@@ -18,12 +20,67 @@ night.addEventListener("click", () => {
   night.classList.toggle("ri-moon-line");
   night.classList.toggle("ri-sun-line");
 });
-
+//
 function fetchMovie() {
   fetch(`http://www.omdbapi.com/?s=${search.value}&apikey=${API}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      if (data.Response === "True") {
+        console.log(data);
+        populateData(data);
+      } else {
+        console.log("Not able to fetch Movies", data.Error);
+      }
     })
     .catch((err) => console.error(err));
+}
+
+function populateData(data) {
+  container.innerHTML = data.Search.map(
+    (movie) => `
+      <div class="movie-detail">
+        <img src="${movie.Poster}" alt="${movie.Title}">
+        <p>⭐${movie.imdbRating || "N/A"}</p>
+        <h1>${movie.Title}</h1>
+        <p>Year: ${movie.Year}</p>
+        <button>More Info</button>
+      </div>
+    `
+  ).join("");
+
+  container.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+      popup(
+        data.Search.find(
+          (movie) =>
+            movie.Title === event.target.parentNode.children[2].textContent
+        )
+      );
+      popupContainer.classList.add("show");
+    }
+  });
+}
+
+function popup(movie) {
+  const popupData = document.querySelector(".moreInfo");
+  popupData.innerHTML = "";
+  popupData.innerHTML = `
+      <i class="ri-close-large-line" id="test"></i>
+      <img src="${movie.Poster}" alt="${movie.Title}">
+      <h1>${movie.Title}</h1>
+      <p>Year: ${movie.Year}</p>
+      <span>${movie.Runtime}</span>
+      <span>${movie.Rated}</span>
+      <p>Released: ${movie.Released}</p>
+      <p>Genre: ${movie.Genre}</p>
+      <p>⭐${movie.imdbRating}/10</p>
+      <h6>${movie.Plot}</h6>
+      <p>Director: ${movie.Director}</p>
+      <p>Actors: ${movie.Actors}</p>
+      <p>Writer: ${movie.Writer}</p>
+      <p>Awards: ${movie.Awards}</p>`;
+
+  document.querySelector("#test").addEventListener("click", () => {
+    popupContainer.classList.remove("show");
+  });
 }
