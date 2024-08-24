e47a187ec35787748d574e5eec9f29ab337f3f2e
Ankit Singh <ankitsingh635101@gmail.com>
2024-08-24 13:41:49 +0530

    revamped ui and added logic to handle edit and delete button

--- Day-27-Task-Managment-App/script.js
+++ Day-27-Task-Managment-App/script.js
@@ -1,62 +1,94 @@
+//Selecting all elements
 const form = document.querySelector("form");
-const i = document.querySelector("i");
-const input = document.querySelectorAll("input");
-const formData = document.querySelector(".formData");
-const container = document.querySelector(".container");
-const button = document.querySelector("button");
-const arr = [];
+const title = document.querySelector("#title");
+const desc = document.querySelector("#description");
+const date = document.querySelector("#date");
+const taskList = document.querySelector(".taskList");
+const upDown = document.querySelector(".upDown");
+let taskEditable = null;
+const tasksCount = [];
 
-i.addEventListener("click", () => {
+upDown.addEventListener("click", () => {
   form.classList.toggle("collapse");
-  i.classList.toggle("ri-arrow-up-s-line");
-  i.classList.toggle("ri-arrow-down-s-line");
 });
 
-button.addEventListener("click", (event) => {
+//adding event lister to form and prevernt default
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const task = {};
+  const task = {
+    title: title.value,
+    desc: desc.value,
+    date: date.value,
+  };
+  if (tasksCount.length < 3) {
+    if (taskEditable) {
+      //updating the edit task
+      document.querySelector("h2").textContent = task.title;
+      document.querySelector("p:first-of-type").textContent = task.desc;
+      document.querySelector("p:last-of-type").textContent = task.date;
+      taskEditable = null;
+    } else {
+      tasksCount.push(task);
+      //   localStorage.setItem(task, JSON.stringify(task));
+      // Will implement local storage later
+      createTask(task);
+    }
+  } else {
+    confirm(
+      "You have exceed the maxium no of tasks. Please deleted previous task to add new task."
+    );
+  }
 
-  input.forEach((input) => {
-    task[input.name] = input.value;
-  });
-  arr.push(task);
-  form.reset();
   form.classList.toggle("collapse");
-  displayData(task);
+  title.value = "";
+  desc.value = "";
+  date.value = "";
 });
 
-function displayData(task) {
-  formData.innerHTML += `
-  <div class="taskItem">
-  <span class="menu-icon">...</span>
-   <div class="edit">
-   <button class="editBtn">Edit</button>
-   <button class="deleteBtn">Delete</button>
-   </div>
-  <div class="tasks">
-    <p>Title: ${task.title || "Not Provided"}</p>
-  <p>Description: ${task.description || "Not Provided"}</p>
-  <p>Date of Creation:${task.date || "Not Provided"}</p>
-  </div>
-  </div>
-  `;
+function createTask(task) {
+  taskList.innerHTML += `
+    <div class="tasks">
+    <div class="taskItem">
+    <h2>${task.title}</h2>
+    <div class="line-break"></div>
+    <p>${task.desc}</p>
+    <p>${task.date}</p>
+     </div>
+     <div class="icons">
+      <i class="ri-edit-box-line"></i>
+      <i class="ri-delete-bin-fill"></i>
+     </div>
+    </div>
+    `;
+  const deleteIcon = document.querySelectorAll(".ri-delete-bin-fill");
+  deleteIcon.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      const remove = event.target.closest(".tasks");
+      const result = confirm("Are you sure to delete?");
+      if (result) {
+        console.log(result);
+        remove.remove();
+      }
+    });
+  });
+  const editIcon = document.querySelectorAll(".ri-edit-box-line");
+  editBtn(editIcon);
 }
 
-formData.addEventListener("click", (event) => {
-  if (event.target.classList.contains("menu-icon")) {
-    const editMenu = event.target.nextElementSibling;
-    editMenu.classList.toggle("show");
-  }
-  if (event.target.classList.contains("editBtn")) {
-    const taskItem = event.target.closest(".taskItem");
-    const getValues = taskItem.querySelectorAll(".tasks p");
-    getValues.forEach((item) => console.log(item));
-  }
-  if (event.target.classList.contains("deleteBtn")) {
-    const confirmDelete = confirm("Are you sure you want to delete?");
-    if (confirmDelete) {
-      const taskItem = event.target.closest(".taskItem");
-      taskItem.remove();
-    }
-  }
-});
+function editBtn(editIcon) {
+  editIcon.forEach((items) => {
+    items.addEventListener("click", (event) => {
+      let edit = event.target.closest(".tasks");
+      form.classList.toggle("collapse");
+      //Getting the current value
+      const editTitle = document.querySelector("h2").textContent;
+      const editDesc = document.querySelector("p:first-of-type").textContent;
+      const editDate = document.querySelector("p:last-of-type").textContent;
+      //populating the current value to fileds
+      title.value = editTitle;
+      desc.value = editDesc;
+      date.value = editDate;
+      taskEditable = edit;
+    });
+  });
+}
